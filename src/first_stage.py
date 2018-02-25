949c993391954cede1cd532258e1574a1c0ede6b
Kaustubh "Kauby" Sakhalkar <sakhalkark@gmail.com>
2018-02-25 14:42:35 +0100

    Change To INT to match correct datatype

--- src/first_stage.py
+++ src/first_stage.py
@@ -26,7 +26,7 @@ def run_first_stage(image, net, scale, threshold):
     # scale the image and convert it to a float array
     width, height = image.size
     sw, sh = math.ceil(width*scale), math.ceil(height*scale)
-    img = image.resize((sw, sh), Image.BILINEAR)
+    img = image.resize((int(sw), int(sh)), Image.BILINEAR)
     img = np.asarray(img, 'float32')
 
     img = Variable(torch.FloatTensor(_preprocess(img)), volatile=True)
